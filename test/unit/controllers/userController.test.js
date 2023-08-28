ea77d2fabd14cddb20d465fa02546c60d9dac9f7
Aaron J. Brown <39014977+aaronjosephbrown@users.noreply.github.com>
2023-08-27 20:46:47 -0500

    Update

--- test/unit/controllers/userController.test.js
+++ test/unit/controllers/userController.test.js
@@ -1,5 +1,6 @@
 import supertest from 'supertest'
 import app from '../../../index.js'
+import { faker } from '@faker-js/faker'
 
 const request = supertest(app)
 
@@ -12,25 +13,32 @@ describe('This is a get request for a list of test users.', () => {
 })
 
 describe('This is a post request to create a new user.', () => {
-  it(`return an Joe Rogan object with a status 201`, async () => {
+  it(`return an test user object with a status 201`, async () => {
+    const testUserName = faker.internet.userName()
+    const testFirstName = faker.person.firstName()
+    const testLastName = faker.person.lastName()
+    const testDOB = `08-11-1967`
+    const testEmail = faker.internet.email()
+    const testPassword = faker.internet.password()
+
     const response = await request.post('/users').send({
-      username: 'joerogan',
-      firstname: 'Joe',
-      lastname: 'Rogan',
-      DOB: '08-11-1967',
-      email: 'joerogan@gmail.com',
-      password: '123@@troN',
-      confirmPassword: '123@@troN',
+      username: testUserName,
+      firstname: testFirstName,
+      lastname: testLastName,
+      DOB: testDOB,
+      email: testEmail,
+      password: testPassword,
+      confirmPassword: testPassword,
     })
     expect(response.statusCode).toBe(201)
     expect(response.body).toEqual({
-      username: 'joerogan',
-      firstname: 'Joe',
-      lastname: 'Rogan',
-      DOB: '08-11-1967',
-      email: 'joerogan@gmail.com',
-      password: '123@@troN',
-      confirmPassword: '123@@troN',
+      username: testUserName,
+      firstname: testFirstName,
+      lastname: testLastName,
+      DOB: testDOB,
+      email: testEmail.toLowerCase(),
+      password: testPassword,
+      confirmPassword: testPassword,
     })
   })
 })
@@ -48,5 +56,23 @@ describe('This is a post request to create a new user without email.', () => {
     })
     expect(response.statusCode).toBe(400)
     expect(response.body.errors[0].msg).toBe('Email is invalid.')
+  })
+})
+
+describe('This is a post request to create a new user without password.', () => {
+  it(`return an error message with a status 400`, async () => {
+    const response = await request.post('/users').send({
+      username: 'joerogan',
+      firstname: 'Joe',
+      lastname: 'Rogan',
+      DOB: '08-11-1967',
+      email: 'joerogan@gmail.com',
+      password: '',
+      passwordConfirm: '',
+    })
+    expect(response.statusCode).toBe(400)
+    expect(response.body.errors[0].msg).toBe(
+      'Password must be at least 8 characters.'
+    )
   })
 })
